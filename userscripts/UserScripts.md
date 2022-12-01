d8c92f4091cf660413bcfbdb5c285d7d47d711b9
dualword <dualword@users.noreply.github.com>
2022-12-01 10:14:29 +0300

    userscripts

--- userscripts/UserScripts.md
+++ userscripts/UserScripts.md
@@ -5,9 +5,11 @@ Userscripts (~/.config/qupzilla/profiles/default/extensions/greasemonkey):
 
 youtube.com
  - youtube-expand-description.js
+ - youtube-autoplay-off.js
  - youtube-hide-comments.js
  - youtube-hide-recommended.js
- - youtube-autoplay-off.js
+ - youtube-hide-signedout.js
+
 
 
 
---
+++ userscripts/youtube-hide-signedout.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name          youtube-hide-signedout
+// @description   Hides "you are signed out" messagebox
+// @match         https://www.youtube.com/watch*
+// @author        https://github.com/dualword/Qupzilla-Qt/
+// @namespace     https://github.com/dualword/Qupzilla-Qt/
+// @version       0.1
+// ==/UserScript==
+
+var id = 'button[aria-label="Got it"]';
+t = window.setInterval(test, 3000);
+var i = 0, m = 30;
+
+function test(){
+	var o = document.querySelector(id);
+	if (o) {
+		o.click();
+		window.clearInterval(t);
+		return;
+	}
+	if (i++ >= m) window.clearInterval(t);
+}
+

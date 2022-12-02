c3dc69bde9dd87f279d6cf613543ac834f475e74
dualword <dualword@users.noreply.github.com>
2022-12-02 16:23:21 +0300

    userscripts

--- userscripts/UserScripts.md
+++ userscripts/UserScripts.md
@@ -1,16 +1,13 @@
 
 Userscripts (~/.config/qupzilla/profiles/default/extensions/greasemonkey):  
-
 ---
-
 youtube.com
  - youtube-expand-description.js
  - youtube-autoplay-off.js
+ - youtube-toggle-comments.js
  - youtube-hide-comments.js
  - youtube-hide-recommended.js
  - youtube-hide-signedout.js
-
-
 
 
  
---
+++ userscripts/youtube-toggle-comments.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name          youtube-toggle-comments
+// @description   Toggles comments
+// @match         https://www.youtube.com/watch*
+// @author        https://github.com/dualword/Qupzilla-Qt/
+// @namespace     https://github.com/dualword/Qupzilla-Qt/
+// @version       0.1
+// ==/UserScript==
+
+var o;
+var id = "comments";
+new MutationObserver(function(m) {
+    if (document.getElementById(id)) {
+        o = document.getElementById(id);
+		var link = document.createElement('a');
+		link.innerHTML = '<center><a style="font-size:2em;" id="toggle-comments" href="#toggle-comments">Toggle comments</a></center>';
+		o.parentNode.insertBefore(link.firstChild, o);
+		document.getElementById("toggle-comments").addEventListener("click", toggle, true);
+		o.style.display = "none";
+		this.disconnect();
+    }
+}).observe(document, {childList: true, subtree: true});
+
+function toggle(event){
+	if(event) {
+		event.stopPropagation();
+		event.preventDefault();
+	  }
+	if(o.classList.contains("inline-block")) {
+		o.style.display = "none";
+	}else{
+		o.style.display = "inline-block";
+	}
+	o.classList.toggle("inline-block");
+	return false;
+}
+
+
